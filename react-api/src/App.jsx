a7ee4f048fca79564a48f38e5396d559c2effc03
Steopa2001 <moraru495@gmail.com>
2025-09-02 16:32:31 +0200

    feat: enter card information

--- react-api/src/App.jsx
+++ react-api/src/App.jsx
@@ -23,20 +23,40 @@ function App() {
       <div className="row">
         {actors.map((actor, index) => (
           <div key={index} className="col-md-4 mb-4">
-            <div className="card">
+            <div className="card h-100">
+
+              {/* Nome Attore */}
+              <h2 className="text-center text-white">{actor.name}</h2>
+
               {/* Immagine attore */}
-              <img
-              src={actor.image}
-              alt={actor.name}
-              />
+              <img src={actor.image} alt={actor.name} />
+
+              {/* Anno di nascita e nazionalità */}
+              <div className="d-flex justify-content-center">
+                <h6 className="me-2 text-white">{actor.birth_year}</h6>
+                <h6 className="text-white">{actor.nationality}</h6>
+              </div>
+              {/* Biografia */}
+              <p className="text-white text-center">{actor.biography}</p>
+              {/* Noto per */}
+              <div className="text-center text-danger">
+              <span><em><strong>Known for:</strong></em></span>
+              <br/>
+              <span><em>{actor.known_for}</em></span>
+              </div>
+              {/* Riconoscimenti */}
+                <div className="text-center text-warning">
+              <span><em><strong>Awards:</strong></em></span>
+              <br/>
+              <span><em>{actor.awards}</em></span>
+              </div>
             </div>
           </div>
         ))}
         <div className="col"></div>
       </div>
     </div>
-  )
-
+  );
 }
 
 export default App;
