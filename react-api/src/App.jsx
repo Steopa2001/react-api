4bec3876a13c429d18eff68bb5426b67f532ddc5
Steopa2001 <moraru495@gmail.com>
2025-09-02 15:42:31 +0200

    feat: make an Ajax call with Axios and display in console

--- react-api/src/App.jsx
+++ react-api/src/App.jsx
@@ -1,11 +1,23 @@
 //Importiamo hook react 
 import { useEffect, useState } from "react";
+//importo axios
+import axios from 'axios';
 
 function App() {
 
   //Stato lista attori 
   const [actors, setActors] = useState([]);
 
+  useEffect(() => {
+    //richiesta attori
+    axios.get('https://lanciweb.github.io/demo/api/actors/').then(res => {
+      console.log('Actors:', res.data)
+      //Salvo nello stato
+      setActors(res.data)
+    })
+    //eseguo solo al primo caricamento
+  }, []);
+
 }
 
-export default App
+export default App;
